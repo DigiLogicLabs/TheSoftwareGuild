4c606f0e32c9dcd8df0e305623b94b77a67e9c4b
Conner Soligny <csharpener22@gmail.com>
2017-05-09 13:08:38 -0600

    t Please enter the commit message for your changes. Lines starting

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+                Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+
+            for (int i = 1; i <= 100; i++)
+
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if (fizz && buzz)
+
+                    Console.WriteLine(i + " FizzBuzz ");
+
+                else if (fizz)
+
+                    Console.WriteLine(i + " Fizz ");
+
+                else if (buzz)
+                 Console.WriteLine(i + " Buzz ");
+
+                else
+                        Console.WriteLine(i); 
+            }
+ 
+            //TODO:  Implement FizzBuzz
+        }
+    }
+}
