7e425d0d5040b6ebf6303c3a9cab7a7289ace323
Conner Soligny <csharpener22@gmail.com>
2017-06-02 12:28:00 -0600

    Finished SGBank with all tests passing, I believe

--- SGBank.UI/SGBank.BLL/AccountManagerFactory.cs
+++ SGBank.UI/SGBank.BLL/AccountManagerFactory.cs
@@ -6,6 +6,7 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using SGBank.Data;
+using SGBank.Data.Application;
 
 namespace SGBank.BLL
 {
@@ -15,7 +16,7 @@ namespace SGBank.BLL
          public static AccountManager Create()
             //reference added in assemblies(.NET framework) = system.configuration has a Class in it ConfigurationManager which reads your .config file
          {
-             string mode = ConfigurationManager.AppSettings["BasicTest"].ToString(); //Opens up appconfig, and looks at app setting and pulls value assigned to the Mode key and puts it into the string
+             string mode = ConfigurationManager.AppSettings["Mode"].ToString(); //Opens up appconfig, and looks at app setting and pulls value assigned to the Mode key and puts it into the string
 
              switch (mode)
              {
@@ -25,6 +26,8 @@ namespace SGBank.BLL
                     return new AccountManager(new BasicAccountTestRepository());
                 case "PremiumTest":
                     return new AccountManager(new PremiumAccountTestRepository());
+                case "Accounts.txt":
+                    return new AccountManager(new FileAccountRepository());
 
                 default:
                     throw new Exception("Mode value in app config is not valid ");
---
+++ SGBank.UI/SGBank.Data/ApplicationLiveData/FileAccountRepository.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGBank.Models;
+using SGBank.Models.Interfaces;
+using System.IO;
+
+namespace SGBank.Data.Application
+{
+    
+    public class FileAccountRepository : IAccountRepository
+    {
+        private const string _filePath = @"C:\Users\Csharpener\Desktop\Repos\conner-soligny-individual-work\SGBank.UI\Accounts.txt";
+
+
+        public Account LoadAccount(string AccountNumber)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(_filePath))
+                {
+                    reader.ReadLine(); // skip the header
+                    string line;
+
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+
+                        string[] entry = line.Split(',');
+
+                        if (entry [0] == AccountNumber)
+                        {
+
+                            return ParseToAccount(entry);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error opening up file: " + exception.Message); 
+            }
+            return null;
+        }
+
+        public void SaveAccount(Account account)
+        {
+            try
+            {
+                List<string> lines = new List<string>();
+                using (StreamReader reader = new StreamReader(_filePath))
+                {
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] entriesStrings = line.Split(',');
+
+                        if (entriesStrings[0] == account.AccountNumber)
+                        {
+                            lines.Add(ParseFromAccount(account));
+                        }
+                        else
+                        {
+                            lines.Add(line);
+                        }
+                    }
+                    
+                }
+                File.WriteAllLines(_filePath, lines);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error saving to File: " + exception.Message);
+                
+            }
+            
+        }
+
+        private Account ParseToAccount(string[] entry)
+        {
+            AccountType type;
+
+            switch (entry[3])
+            {
+                case "F":
+                    type = AccountType.Free;
+                    break;
+
+                case "B":
+                    type = AccountType.Basic;
+                    break;
+                case "P":
+                    type = AccountType.Premium;
+                    break;
+                default:
+                    throw new Exception("Account Type doesn't match");
+            }
+            return new Account()
+            {
+                AccountNumber = entry[0],
+                Name = entry[1],
+                Balance = decimal.Parse(entry[2]),
+                Type = type
+            };
+
+        }
+        private string ParseFromAccount(Account account)
+        {
+            return $"{account.AccountNumber},{account.Name},{account.Balance},{account.Type.ToString()[0]}";
+        }
+    }
+}
